e16c68556c1a6fb2480af3d30f209012edc8013a
Joel D'Autilio <joeldautilio@gmail.com>
2024-01-31 16:47:57 -0300

    tp1: cross origin

--- ejercicio1/api/src/main/java/ayiacademy/tp2/ejercicio1backend/controllers/LoginController.java
+++ ejercicio1/api/src/main/java/ayiacademy/tp2/ejercicio1backend/controllers/LoginController.java
@@ -5,10 +5,12 @@ import ayiacademy.tp2.ejercicio1backend.services.UsuariosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class LoginController {
     @Autowired
