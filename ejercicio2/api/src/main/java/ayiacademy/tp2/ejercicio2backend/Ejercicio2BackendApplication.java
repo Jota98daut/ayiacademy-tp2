ebeef06848da401c8a26e19998041b7d25a19feb
Joel D'Autilio <joeldautilio@gmail.com>
2024-02-01 15:23:25 -0300

    ej2: renombrar proyecto

--- ejercicio2/api/src/main/java/ayiacademy/tp2/ejercicio2backend/Ejercicio2BackendApplication.java
+++ ejercicio2/api/src/main/java/ayiacademy/tp2/ejercicio2backend/Ejercicio2BackendApplication.java
@@ -1,13 +1,13 @@
-package ayiacademy.tp2.ejercicio1backend;
+package ayiacademy.tp2.ejercicio2backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ejercicio1BackendApplication {
+public class Ejercicio2BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ejercicio1BackendApplication.class, args);
+		SpringApplication.run(Ejercicio2BackendApplication.class, args);
 	}
 
 }
