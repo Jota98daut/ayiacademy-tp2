ed05d53b937c4e24868447b856f7277c22b2a1af
Joel D'Autilio <joeldautilio@gmail.com>
2024-02-01 15:11:38 -0300

    ej2: inicializar backend

---
+++ ejercicio2/api/src/main/java/ayiacademy/tp2/ejercicio1backend/Ejercicio1BackendApplication.java
@@ -0,0 +1,13 @@
+package ayiacademy.tp2.ejercicio1backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio1BackendApplication.class, args);
+	}
+
+}
---
+++ ejercicio2/api/src/test/java/ayiacademy/tp2/ejercicio1backend/Ejercicio1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ayiacademy.tp2.ejercicio1backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
